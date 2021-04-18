6a308ea3080b19db9db44b73642a2b5262dc7eac
Dinh Toan <toank61humg@gmail.com>
2021-04-18 09:56:39 +0700

    test prettier lint-staged husky

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,9 +2,9 @@ const coursesRouter = require('./courses.route');
 const siteRouter = require('./site.route');
 
 function route(app) {
-                                            app.use('/courses', coursesRouter);
+        app.use('/courses', coursesRouter);
 
-    app.use('/', siteRouter);
+    app.use('/',    siteRouter);
 }
 
 module.exports = route;
